3ee6bbde7c7cff5393584d2cfaa434a3b24787ec
Cristian Zaharia <dmg_godkiller@yahoo.com>
2018-01-13 19:24:41 +0000

    Finished project

--- javascript.js
+++ javascript.js
@@ -1,27 +1,55 @@
 const container = document.createElement('div');
-let row
+const btn = document.createElement('button');
+btn.textContent = 'New Grid';
+let cell;
+let input = 4;
+let cells = container.childNodes;
 
-
+// makes container and button and adds them
 container.classList.add('container');
 document.body.appendChild(container);
+btn.classList.add('btn');
+document.body.appendChild(btn);
 
-for (let x = 0; x < 16; x++){
-    row = document.createElement('div');
-    row.classList.add('row');
-    container.appendChild(row);
+function userInput(){
+    return input = +prompt('Grid size?', `${input}` || 4) || 4;
 }
 
-function sketch(row){
-    row = row.target;
-    console.log(row);
-    row.classList.add('row-colored');
-    row.setAttribute('style', `background-color: rgb(${randomsColor()}, ${randomsColor()}, ${randomsColor()}`);
+function makeGrid(){
+    for (let x = 0; x < input * input; x++){
+        cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.setAttribute('style', `grid-template-columns: repeat(${input}, 1fr);`);
+        container.appendChild(cell);
+    }
+}
+
+function removeGrid(){
+    while(container.lastChild){
+        container.removeChild(container.lastChild);
+    }
+}
+
+function makeNewGrid(){
+    removeGrid();
+    makeGrid(userInput());
+    cells.forEach(cell => cell.addEventListener('mouseover', sketch));
+}
+
+function sketch(cell){
+    cell = cell.target;
+    cell.setAttribute('style', `background-color: rgb(${randomsColor()}, ${randomsColor()}, ${randomsColor()}`);
 }
 
 function randomsColor(){
     return Math.floor((Math.random() * 255));
 }
 
-let rows = container.childNodes;
+// makes grid on page load
+makeGrid();
 
-rows.forEach(row => row.addEventListener('mouseover', sketch));
+// make new grid on button click
+btn.addEventListener('click', makeNewGrid);
+
+// selects all cells and sketches on mouseover
+cells.forEach(cell => cell.addEventListener('mouseover', sketch));
